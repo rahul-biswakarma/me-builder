6abdae80c333dbef0c2a4c1aa4bd1edadd183296
Rahul Biswakarma <rahul.id39@gmail.com>
2024-09-18 01:49:51 +0530

    added theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Product } from './product';
 
+import './global.css';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Product />
